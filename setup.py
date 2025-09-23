8690b4c212ab57cbad3b0c4349620091cd752d15
unknown <anasshaikh129229@gmail.com>
2025-09-24 04:53:58 +0530

    Added Pinecone index creation code for medical_chatbot

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author=" Ishaque Anas Shaikh",
+    author_email="anasshaikh129229@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        
+    ]
+)
